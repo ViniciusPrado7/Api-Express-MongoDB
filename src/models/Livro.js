31ea3c718dfdbc2fbaa67a21cb6f48aa334026ce
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2025-10-20 13:49:05 -0300

    feat: criando validações personalizadas.

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,11 +3,33 @@ import mongoose from "mongoose";
 const livroSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Schema.Types.ObjectId },
-    titulo: { type: String, required: [true, "O título do livro é obrigatório"] },
-    editora: { type: String, required: [true, "A editora é obrigatória"]},
+    titulo: {
+      type: String,
+      required: [true, "O título do livro é obrigatório"],
+    },
+    editora: {
+      type: String,
+      enum: {
+        values: ["Casa do Código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido.",
+      },
+      required: [true, "A editora é obrigatória"],
+    },
     preco: { type: Number },
-    paginas: { type: Number },
-    autor: { type: mongoose.Schema.Types.ObjectId, ref: "autores", required: [true, "O(a) autor(a) é obrigatório"]},
+    paginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      },
+    },
+    autor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "autores",
+      required: [true, "O(a) autor(a) é obrigatório"],
+    },
   },
   { versionKey: false }
 );
